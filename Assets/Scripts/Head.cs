fe480ec6a41f55e8d24b25320b1dfe63e223234c
Bolicrad <815763627@qq.com>
2022-09-05 23:03:08 -0600

    Debug

---
+++ Assets/Scripts/Head.cs
@@ -0,0 +1,243 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : MonoBehaviour
+{
+    public enum PoopEffectType
+    {
+        ReduceLength,
+        ReverseInput,
+        LostControl,
+        Speedup
+    }
+
+    public enum SnakeDieReason
+    {
+        HitSelf, 
+        HitWall, 
+        Length0, 
+        PoopSnake
+    };
+    private static System.Random _rng = new System.Random();
+    private Vector2 now; // actual head direction
+    private Vector2 next;
+    private int angle;
+    private Transform TailTransform => bodyParent.childCount > 0 ? bodyParent.GetChild(bodyParent.childCount - 1) : null;
+    public Transform bodyParent;
+    public GameObject bodyPrefab;
+    public GameObject foodPrefab;
+    public GameObject poopPrefab;
+    public GameObject food;
+    private Vector3 tmp;
+    private bool poopCounting = false;
+    private int poopCount = 0;
+    public int digestMoveNumber = 3;
+    private int poopDamage = 1;
+    private bool canInput = true;
+    public float unitScale = 0.5f;
+    public float timer = 0f;
+    public float defaultTimerGap = 0.1f;
+    private int speedlevel = 1;
+    public int debuffTime = 1;
+    private bool lostControl = false;
+    private bool reverseInput = false;
+    private Coroutine lostControlHandler = null;
+    private Coroutine reverseInputHandler = null;
+    public int score = 0;
+
+    public void Start()
+    {
+        for (var i = 0; i < 3; ++i)
+        {
+            CreateBody(transform.position - (i + 1) * new Vector3(0, unitScale, 0));
+        }
+        now = Vector2.up;
+        next = Vector2.up;
+        angle = 0;
+        Manager.manager.CreateFood();
+    }
+    
+    private void CreatePoop(Vector3 position)
+    {
+        Instantiate(poopPrefab, position, Quaternion.identity);
+    }
+
+    private void CreateBody(Vector3 position)
+    {
+        var newBody = Instantiate(bodyPrefab, position, Quaternion.identity);
+        newBody.transform.SetParent(bodyParent);
+    }
+
+    private IEnumerator DeleteBody(int num)
+    {
+        for (var i = 0; i < num; i++)
+        {
+            yield return null;
+            RemoveBody();
+        }
+    }
+
+    private IEnumerator LostControlDebuff(int time)
+    {
+        lostControl = true;
+        yield return new WaitForSeconds(time);
+        lostControl = false;
+    }
+
+    private IEnumerator ReverseInputDebuff(int time)
+    {
+        reverseInput = true;
+        yield return new WaitForSeconds(time);
+        reverseInput = false;
+    }
+
+    private void RemoveBody()
+    {
+        if (TailTransform)
+        {
+            Destroy(TailTransform.gameObject);
+        }
+        else
+        {
+            Manager.manager.SnakeDie(SnakeDieReason.Length0);
+        }
+
+    }
+
+    private void MoveBody()
+    {
+        tmp = transform.position;
+        
+        transform.position = unitScale * now + (Vector2)transform.position;
+
+        if (poopCounting)
+        {
+            if (poopCount > 0)
+            {
+                poopCount--;
+            }
+            else
+            {
+                CreatePoop(TailTransform ? TailTransform.position : tmp);
+                poopCounting = false;
+            }
+        }
+        if (TailTransform)
+        {
+            TailTransform.position = tmp;
+            TailTransform.SetAsFirstSibling();
+        }
+        transform.eulerAngles = angle * Vector3.forward;
+    }
+
+    private void Update()
+    {
+        if (canInput && !lostControl)
+        {
+            float input;
+            if (now == Vector2.up || now == Vector2.down) {
+                input = Input.GetAxis("Horizontal");
+                if (Mathf.Abs(input) > 0) {
+                    now = (reverseInput?input<0:input>0) ? Vector2.right : Vector2.left;
+                    angle = (reverseInput ? input < 0 : input > 0) ? -90 : 90;
+                    canInput = false;
+                }
+            }
+            if (now == Vector2.left || now == Vector2.right) {
+                input = Input.GetAxis("Vertical");
+                if (Mathf.Abs(input) > 0) {
+                    now = (reverseInput ? input < 0 : input > 0) ? Vector2.up : Vector2.down;
+                    angle = (reverseInput ? input < 0 : input > 0) ? 0 : 180;
+                    canInput = false;
+                }
+            }
+        }
+
+        timer += Time.deltaTime;
+
+
+        if (timer > defaultTimerGap / speedlevel)
+        {
+            MoveBody();
+            canInput = true;
+            timer = 0;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Enter: "+other.tag);
+        if (other.tag.Equals("Food"))
+        {
+            score += speedlevel;
+            Manager.manager.scoreText.text = $"Score: {score}";
+            Manager.manager.PlayAudio(1);
+            Destroy(food);
+            food = null;
+            CreateBody(TailTransform ? TailTransform.position : transform.position);
+            poopCounting = true;
+            poopCount = digestMoveNumber;
+            Manager.manager.CreateFood();
+        }
+
+        if (other.tag.Equals("Poop"))
+        {
+            Destroy(other.gameObject);
+            var damageType = (PoopEffectType)_rng.Next(System.Enum.GetNames(typeof(PoopEffectType)).Length);
+            int option = 0;
+            switch (damageType)
+            {
+                case PoopEffectType.ReduceLength:
+                    {
+                        option = poopDamage;
+                        StartCoroutine(DeleteBody(poopDamage));
+                        poopDamage++;
+                        break;
+                    }
+                case PoopEffectType.ReverseInput:
+                    {
+                        option = debuffTime;
+                        if (reverseInputHandler != null)
+                        {
+                            StopCoroutine(reverseInputHandler);
+                            reverseInputHandler = null;
+                        }
+                        reverseInputHandler = StartCoroutine(ReverseInputDebuff(debuffTime));
+                        break;
+                    }
+                case PoopEffectType.LostControl:
+                    {
+                        option = debuffTime;
+                        if (lostControlHandler != null)
+                        {
+                            StopCoroutine(lostControlHandler);
+                            lostControlHandler = null;
+                        }
+                        lostControlHandler = StartCoroutine(LostControlDebuff(debuffTime));
+                        break;
+                    }
+                case PoopEffectType.Speedup:
+                    {
+                        speedlevel++;
+                        option = speedlevel;
+                        break;
+                    }
+            }
+            Manager.manager.TellPoopEffect(damageType, option);
+        }
+
+        if (other.tag.Equals("Body"))
+        {
+            Manager.manager.SnakeDie(SnakeDieReason.HitSelf);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Exit: "+other.tag);
+        if (other.tag.Equals("Boundary")&&transform.gameObject.activeInHierarchy)
+        {
+            Manager.manager.SnakeDie(SnakeDieReason.HitWall);
+        }
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager manager;
+    public GameObject headPrefab;
+    public BoxCollider2D border;
+    public SpriteRenderer spriteRenderer;
+    public TMP_Text deathText;
+    public List<TMP_Text> effectTexts;
+    public TMP_Text scoreText;
+    public Button startButton;
+    private Head head;
+    private Vector3 borderSize;
+    public AudioClip[] audioClips;
+    public AudioSource audioSource;
+    private Coroutine lEDHandler;
+
+    private void Awake()
+    {
+        manager = this;
+        borderSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        border.size = new Vector2(borderSize.y * 2 - 1.5f, borderSize.y * 2 - 1.5f);
+        spriteRenderer.size = new Vector2(borderSize.y * 2 - 0.5f, borderSize.y * 2 - 0.5f);
+        transform.position = new Vector3(borderSize.y - borderSize.x, 0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startGame();
+    }
+
+    public void StartGame() {
+        head = Instantiate(headPrefab).GetComponent<Head>();
+        head.transform.position = transform.position;
+        head.bodyParent = transform;
+        startButton.gameObject.SetActive(false);
+        deathText.text = string.Empty;
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SnakeDie(Head.SnakeDieReason reason) {
+        var highScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (head.score > highScore) {
+            highScore = head.score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+        }
+        Time.timeScale = 0;
+
+        var text = reason switch
+        {
+            Head.SnakeDieReason.HitSelf => "You ate your body.",
+            Head.SnakeDieReason.HitWall => "You hit the boundary.",
+            Head.SnakeDieReason.Length0 => "Your body fell into the void.",
+            Head.SnakeDieReason.PoopSnake => "The spirit of Poop ate you.",
+            _ => ""
+        };
+        scoreText.text = $"Score: {head.score} \nHigh Score: {highScore}";
+        deathText.text = text;
+        PlayAudio(5);
+        startButton.gameObject.SetActive(true);
+        StartCoroutine(DeleteAllTail());
+        Destroy(head.food);
+        head.gameObject.SetActive(false);
+        foreach (var tmpText in effectTexts)
+        {
+            tmpText.text = string.Empty;
+        }
+        Destroy(head.gameObject);
+    }
+
+    public void TellPoopEffect(Head.PoopEffectType type,int option) {
+        string content = "Poop Effect: ";
+        bool isOption = false;
+        switch (type) {
+            case Head.PoopEffectType.ReduceLength:
+                content += $"Reduced your length by {option}.";
+                PlayAudio(4);
+                break;
+            case Head.PoopEffectType.ReverseInput:
+                isOption = true;
+                content += $"Input Axis reversed for {option}s";
+                PlayAudio(0);
+                break;
+            case Head.PoopEffectType.LostControl:
+                isOption = true;
+                content += $"Lost Control for {option}s";
+                PlayAudio(3);
+                break;
+            case Head.PoopEffectType.Speedup:
+                content += $"Speed Level Up to {option}";
+                PlayAudio(2);
+                break;
+        }
+        if(isOption)PrintToScreen(effectTexts[(int)type],content,option);
+        else PrintToScreen(effectTexts[(int)type],content);
+    }
+
+    public void PrintToScreen(TMP_Text text,string content, int time = 3) {
+        if (lEDHandler != null)
+        {
+            StopCoroutine(lEDHandler);
+            lEDHandler = null;
+        }
+
+        lEDHandler = StartCoroutine(PostLed(text, content, time));
+    }
+
+    private IEnumerator PostLed(TMP_Text text, string content, int time) {
+
+        text.text = content;
+        yield return new WaitForSeconds(time);
+        text.text = "";
+    }
+
+    IEnumerator DeleteAllTail() {
+        while (transform.childCount > 0) {
+            Destroy(transform.GetChild(0).gameObject);
+            yield return null;
+        }
+    }
+    
+    public void CreateFood()
+    {
+        var x = Random.Range(-borderSize.y + 1, borderSize.y - 1);
+        var y = Random.Range(-borderSize.y + 1, borderSize.y - 1);
+        x += transform.position.x;
+        if (!head.food)
+        {
+            head.food = Instantiate(head.foodPrefab, new Vector3(x, y), Quaternion.identity);
+        }
+        else head.food.transform.position = new Vector3(x, y);
+    }
+
+    public void PlayAudio(int index) {
+        // audioSource.clip = audioClips[index];
+        // audioSource.Play();
+    }
+}
